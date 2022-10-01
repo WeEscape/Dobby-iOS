3fd31d17d759fc33f198cd9847c1f78bf7e6ff21
eymin1259 <eymin1259@gmail.com>
2022-10-02 00:20:13 +0900

    FIX : 불필요 파일 및 함수 삭제

--- Dobby/Dobby/Sources/Presentation/Common/BaseViewController.swift
+++ Dobby/Dobby/Sources/Presentation/Common/BaseViewController.swift
@@ -12,16 +12,6 @@ class BaseViewController: UIViewController {
     
     var disposeBag: DisposeBag = .init()
     
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        BeaverLog.debug("\(String(describing: self)) init")
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        BeaverLog.debug("\(String(describing: self)) init")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         BeaverLog.debug("\(String(describing: self)) viewDidLoad")
