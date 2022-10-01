b2c2db00efd327531e7f58864bb869a1609dd5f3
eymin1259 <eymin1259@gmail.com>
2022-10-02 00:25:52 +0900

    ADD : splashViewContrller 로드시 토큰로드

--- Dobby/Dobby/Sources/Presentation/Splash/SplashViewController.swift
+++ Dobby/Dobby/Sources/Presentation/Splash/SplashViewController.swift
@@ -6,12 +6,62 @@
 //
 
 import UIKit
+import RxViewController
 
 final class SplashViewController: BaseViewController {
     
+    var splashViewModel: SplashViewModel
+    weak var splashCoordinator: SplashCoordinator?
+    
+    init(
+        splashViewModel: SplashViewModel,
+        splashCoordinator: SplashCoordinator
+    ) {
+        self.splashViewModel = splashViewModel
+        self.splashCoordinator = splashCoordinator
+        super.init(nibName: nil, bundle: nil)
+        BeaverLog.debug("\(String(describing: self)) init")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+        bind()
+    }
+    
+    func setupUI() {
         view.backgroundColor = .green
     }
+    
+    func bind() {
+        bindState()
+        bindAction()
+    }
+    
+    func bindState() {
+        splashViewModel.isSignIn
+            .subscribe { [weak self] result in
+                if result {
+                    self?.splashCoordinator?.presentMainTab()
+                } else {
+                    self?.splashCoordinator?.presentWelcome()
+                }
+            }.disposed(by: self.disposeBag)
+    }
+    
+    func bindAction() {
+     
+        self.rx.viewDidAppear
+            .subscribe { _ in
+                let splashDuration: DispatchTimeInterval = .seconds(2)
+                DispatchQueue.main.asyncAfter(deadline: .now() + splashDuration) { [weak self] in
+                    self?.splashViewModel.loadAccessToken()
+                }
+            }.disposed(by: self.disposeBag)
+    }
+    
 }
