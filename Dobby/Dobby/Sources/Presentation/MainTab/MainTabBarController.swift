e565c17714198d17cc5bf7bbe7848a79a95c5505
eymin1259 <eymin1259@gmail.com>
2022-10-02 18:18:17 +0900

    ADD : MainTabBarController 생성

---
+++ Dobby/Dobby/Sources/Presentation/MainTab/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  Dobby
+//
+//  Created by yongmin lee on 10/2/22.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
