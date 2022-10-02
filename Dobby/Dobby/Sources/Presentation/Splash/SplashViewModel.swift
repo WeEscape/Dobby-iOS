19ff1e35bc346a1d98a7ceb8da3e0801f8a92b2d
eymin1259 <eymin1259@gmail.com>
2022-10-02 09:52:42 +0900

    ADD : splashViewModel 파일 생성

--- Dobby/Dobby/Sources/Presentation/Splash/SplashViewModel.swift
+++ Dobby/Dobby/Sources/Presentation/Splash/SplashViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+class SplashViewModel {
+    
+    var disposBag: DisposeBag = .init()
+    let isSignIn: PublishRelay<Bool> = .init()
+    let authenticationRepository: AuthenticationRepository
+    
+    init(authenticationRepository: AuthenticationRepository) {
+        self.authenticationRepository = authenticationRepository
+    }
+    
+    func loadAccessToken() {
+        print("debug : SplashViewModel loadAccessToken ")
+    }
+    
+}
