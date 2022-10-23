c51e2fc8dbdd4a7958218bd0405272064f9f3ef2
eymin1259 <eymin1259@gmail.com>
2022-10-23 17:58:43 +0900

    ADD : AddTaskViewModel 생성

---
+++ Dobby/Dobby/Sources/Presentation/AddTask/AddTaskViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  AddTaskViewModel.swift
+//  Dobby
+//
+//  Created by yongmin lee on 10/23/22.
+//
+
+import Foundation
+import RxSwift
+
+class AddTaskViewModel {
+    
+    var disposeBag: DisposeBag = .init()
+    
+    init() {
+        BeaverLog.debug("\(String(describing: self)) init")
+    }
+    
+    deinit {
+        self.disposeBag = .init()
+        BeaverLog.debug("\(String(describing: self)) deinit")
+    }
+}
