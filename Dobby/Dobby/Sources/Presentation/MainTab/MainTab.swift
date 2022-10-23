a48695fe28552ddda765e4e0299a93630641d86c
eymin1259 <eymin1259@gmail.com>
2022-10-23 15:35:01 +0900

    ADD : MainTab 케이스 정의 및 케이스별 TabBarItem 생성함수 구현

--- Dobby/Dobby/Sources/Presentation/MainTab/MainTab.swift
+++ Dobby/Dobby/Sources/Presentation/MainTab/MainTab.swift
@@ -5,4 +5,71 @@
 //  Created by yongmin lee on 10/17/22.
 //
 
-import Foundation
+import UIKit
+
+enum MainTab: Int, CaseIterable {
+    case dailyTask
+    case weeklyTask
+    case addTask
+    case monthlyTask
+    case mypage
+}
+
+extension MainTab: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .dailyTask:
+            return "일간"
+        case .weeklyTask:
+            return "주간"
+        case .addTask:
+            return "등록"
+        case .monthlyTask:
+            return "월간"
+        case .mypage:
+            return "마이페이지"
+        }
+    }
+}
+
+extension MainTab {
+    var icon: UIImage? {
+        switch self {
+        case .dailyTask:
+            return UIImage(systemName: "play")
+        case .weeklyTask:
+            return UIImage(systemName: "pause")
+        case .addTask:
+            return UIImage(systemName: "stop")
+        case .monthlyTask:
+            return UIImage(systemName: "cloud")
+        case .mypage:
+            return UIImage(systemName: "person")
+        }
+    }
+    
+    var selectedIcon: UIImage? {
+        switch self {
+        case .dailyTask:
+            return UIImage(systemName: "play.fill")
+        case .weeklyTask:
+            return UIImage(systemName: "pause.fill")
+        case .addTask:
+            return UIImage(systemName: "stop.fill")
+        case .monthlyTask:
+            return UIImage(systemName: "cloud.fill")
+        case .mypage:
+            return UIImage(systemName: "person.fill")
+        }
+    }
+}
+
+extension MainTab {
+    func getTabBarItem() -> UITabBarItem {
+        return UITabBarItem(
+            title: description,
+            image: icon,
+            selectedImage: selectedIcon
+        )
+    }
+}
