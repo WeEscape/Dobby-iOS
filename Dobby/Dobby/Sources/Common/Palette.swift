d8f2d143234871ba0fcf78d21cbf498ad08e25dc
eymin1259 <eymin1259@gmail.com>
2022-10-27 20:36:06 +0900

    ADD: gray색 추가 및 이름 수정

--- Dobby/Dobby/Sources/Common/Palette.swift
+++ Dobby/Dobby/Sources/Common/Palette.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 struct Palette {
-    static let blue1 = UIColor(
+    static let mainThemeBlue1 = UIColor(
         red: (16.0/255.0), green: (86.0/255.0), blue: (251.0/255.0), alpha: 1.0
     )
-    static let black1 = UIColor(
+    static let kakaoYellow = UIColor(
+        red: 0.97, green: 0.9, blue: 0.0, alpha: 1.0
+    )
+    static let textBlack1 = UIColor(
         red: (65.0/255.0), green: (65.0/255.0), blue: (65.0/255.0), alpha: 1.0
     )
-    static let yellow1 = UIColor(
-        red: 0.97, green: 0.9, blue: 0.0, alpha: 1.0
+    static let textGray1 = UIColor(
+        red: (109.0/255.0), green: (109.0/255.0), blue: (109.0/255.0), alpha: 1
     )
 }
