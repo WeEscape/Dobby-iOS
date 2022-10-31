1ed1bcbf0bd70d7422f027f7d754952aa6a85d6c
eymin1259 <eymin1259@gmail.com>
2022-10-31 22:24:34 +0900

    ADD: MyPageVC 기본 프로퍼티 및 함수 추가

--- Dobby/Dobby/Sources/Presentation/MyPage/MyPageCoordinator.swift
+++ Dobby/Dobby/Sources/Presentation/MyPage/MyPageCoordinator.swift
@@ -11,6 +11,18 @@ final class MyPageCoordinator: Coordinator {
 
     override init(parentCoordinator: Coordinator? = nil, childCoordinators: [Coordinator] = []) {
         super.init(parentCoordinator: parentCoordinator, childCoordinators: childCoordinators)
-        self.viewController = MyPageViewController()
+        let mypageVM = MyPageViewModel(
+            authUseCase: AuthUseCaseImpl(
+                authenticationRepository: AuthenticationRepositoryImpl(
+                    network: NetworkServiceImpl.shared,
+                    localStorage: UserDefaults.standard
+                )
+            )
+        )
+        let mypageVC = MyPageViewController(
+            mypageCoordinator: self,
+            mypageViewModel: mypageVM
+        )
+        self.viewController = mypageVC
     }
 }
--- Dobby/Dobby/Sources/Presentation/MyPage/MyPageViewController.swift
+++ Dobby/Dobby/Sources/Presentation/MyPage/MyPageViewController.swift
@@ -6,11 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MyPageViewController: BaseViewController {
  
+    // MARK: property
+    weak var mypageCoordinator: MyPageCoordinator?
+    let mypageViewModel: MyPageViewModel
+    
+    // MARK: init
+    init(
+        mypageCoordinator: MyPageCoordinator?,
+        mypageViewModel: MyPageViewModel
+    ) {
+        self.mypageCoordinator = mypageCoordinator
+        self.mypageViewModel = mypageViewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupUI()
+        bind()
+    }
+    
+    // MARK: method
+    
+    func setupUI() {
+        self.view.backgroundColor = .white
+        
+        self.navigationItem.title = "설정"
+        self.navigationController?.navigationBar.backgroundColor = .white
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.tintColor = .black
+        self.view.addSubview(navigationLineView)
+        self.navigationLineView.snp.makeConstraints {
+            $0.left.equalToSuperview()
+            $0.right.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+    }
+    
+    func bind() {
+        bindState()
+        bindAction()
+    }
+    
+    func bindState() {
+        
+    }
+    
+    func bindAction() {
+        
     }
 }
