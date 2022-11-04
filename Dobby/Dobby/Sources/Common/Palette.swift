c231223cf8e2e6586ad8c65db60c64ad60adfacf
eymin1259 <eymin1259@gmail.com>
2022-11-04 20:50:49 +0900

    ADD: 프로필 색상 UIColor 추가

--- Dobby/Dobby/Sources/Common/Palette.swift
+++ Dobby/Dobby/Sources/Common/Palette.swift
@@ -22,4 +22,15 @@ struct Palette {
     static let textRed1 = UIColor(red: 1, green: 0.368, blue: 0.368, alpha: 1)
     static let lineGray1 = UIColor(red: 0.921, green: 0.921, blue: 0.921, alpha: 1)
     
+    // profile color
+    static let profileBlue = UIColor(red: 0.063, green: 0.337, blue: 0.984, alpha: 1)
+    static let profileMint = UIColor(red: 0, green: 0.996, blue: 0.961, alpha: 1)
+    static let profileGreen = UIColor(red: 0.165, green: 0.992, blue: 0.592, alpha: 1)
+    static let profilePink = UIColor(red: 1, green: 0.275, blue: 0.623, alpha: 1)
+    static let profilePurple = UIColor(red: 0.887, green: 0.433, blue: 1, alpha: 1)
+    static let profileRed = UIColor(red: 0.992, green: 0.165, blue: 0.165, alpha: 1)
+    static let profileOrange = UIColor(red: 1, green: 0.578, blue: 0.083, alpha: 1)
+    static let profileYellow = UIColor(red: 1, green: 0.84, blue: 0, alpha: 1)
+    static let profileBrown = UIColor(red: 0.654, green: 0.393, blue: 0, alpha: 1)
+    static let profileBlack = UIColor(red: 0, green: 0, blue: 0, alpha: 1)   
 }
