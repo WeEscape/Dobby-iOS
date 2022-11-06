e8f7b30e252430cdc45c90bd97327002277f55d0
eymin1259 <eymin1259@gmail.com>
2022-11-06 20:47:37 +0900

    ADD: 집안일등록화면 반복속성 파일 생성

---
+++ Dobby/Dobby/Sources/Presentation/AddChore/SelectChoreAttributeModal/SelectChoreAttributeView/SelectRepeatCycleView/SelectRepeatCycleCell.swift
@@ -0,0 +1,8 @@
+//
+//  SelectRepeatCycleCell.swift
+//  Dobby
+//
+//  Created by yongmin lee on 11/6/22.
+//
+
+import Foundation
---
+++ Dobby/Dobby/Sources/Presentation/AddChore/SelectChoreAttributeModal/SelectChoreAttributeView/SelectRepeatCycleView/SelectRepeatCycleView.swift
@@ -0,0 +1,38 @@
+//
+//  SelectRepeatCycleView.swift
+//  Dobby
+//
+//  Created by yongmin lee on 11/6/22.
+//
+
+import UIKit
+
+
+final class SelectRepeatCycleView: ModalContentView {
+    
+    // MARK: property
+    
+    
+    // MARK: UI
+    struct Metric {
+        
+    }
+    
+    // MARK: init
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: methods
+    override func setupUI() {
+        super.setupUI()
+    }
+    
+    override func reloadView(_ value: Any?) {
+        
+    }
+}
