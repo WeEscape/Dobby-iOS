29389aa65d8ffe5217ed2a1000f24c3eeb2c75fd
eymin1259 <eymin1259@gmail.com>
2023-01-09 20:35:33 +0900

    FIX: 지원되는 CLKComplicationFamily 수정

--- Dobby/DobbyMini WatchKit Extension/ComplicationController.swift
+++ Dobby/DobbyMini WatchKit Extension/ComplicationController.swift
@@ -14,7 +14,7 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
         let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "Dobby", supportedFamilies: CLKComplicationFamily.allCases)
+            CLKComplicationDescriptor(identifier: "complication", displayName: "Dobby", supportedFamilies: [])
             // Multiple complication support can be added here with more descriptors
         ]
         
