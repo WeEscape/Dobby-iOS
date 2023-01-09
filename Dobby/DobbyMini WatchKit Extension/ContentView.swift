cdd77c0d8008d85cddac6553d64eab8dec5041c8
eymin1259 <eymin1259@gmail.com>
2023-01-09 20:08:59 +0900

    ADD: watch app 프로젝트 생성

---
+++ Dobby/DobbyMini WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DobbyMini WatchKit Extension
+//
+//  Created by yongmin lee on 1/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
