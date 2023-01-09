bbd2e8232acad5d154abb7ed350aa1577e72e944
eymin1259 <eymin1259@gmail.com>
2023-01-09 20:23:01 +0900

    ADD: iOS, watchOS version 통일 -> 1.0.0

--- Dobby/DobbyMini WatchKit Extension/DobbyApp.swift
+++ Dobby/DobbyMini WatchKit Extension/DobbyApp.swift
@@ -16,6 +16,6 @@ struct DobbyApp: App {
             }
         }
 
-        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+//        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
     }
 }
