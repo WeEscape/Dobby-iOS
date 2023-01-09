e8a38be069c4c3d8ed81d28b0c24f1715e4601cb
eymin1259 <eymin1259@gmail.com>
2023-01-09 16:29:13 +0900

    FIX: 로컬데이터 alarmOnOff 타입 변경 bool -> String

--- Dobby/Dobby/Sources/Data/DTO/SettingInfoDTO.swift
+++ Dobby/Dobby/Sources/Data/DTO/SettingInfoDTO.swift
@@ -10,20 +10,20 @@ import Foundation
 struct SettingInfoDTO {
     var accessToken: String
     var refreshToken: String
-    var alarmOnOff: Bool
+    var alarmOnOff: String
     var alarmTime: String
     var userInfo: Data?
     
     init(
         accessToken: String?,
         refreshToken: String?,
-        alarmOnOff: Bool?,
+        alarmOnOff: String?,
         alarmTime: String?,
         userInfo: Data?
     ) {
         self.accessToken = accessToken ?? ""
         self.refreshToken = refreshToken ?? ""
-        self.alarmOnOff = alarmOnOff ?? false
+        self.alarmOnOff = alarmOnOff ?? "0"
         self.alarmTime = alarmTime ?? ""
         self.userInfo = userInfo
     }
